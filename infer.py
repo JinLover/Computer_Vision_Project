1607ac72ea2b6ad4d39ddc8777623f40acbdd96f
JinLover <51523331+JinLover@users.noreply.github.com>
2024-05-25 05:05:11 +0000

    adjust hyperparameter

--- infer.py
+++ infer.py
@@ -19,10 +19,11 @@ from pathlib import Path
 
 images = sorted([str(x) for x in Path("Dataset/test").rglob("*.png")])
 dataloader = _create_data_loader(images, 1, 416, 8)
-model = load_model("config/yolov3-custom.cfg", "checkpoints/yolov3_ckpt_429.pth").cuda()
+model = load_model("config/yolov3-custom.cfg", "checkpoints/yolov3_ckpt_300.pth").cuda()
 model.eval()
 
 pred = []
+# conf_thres = 0.5
 conf_thres = 0.001
 nms_thres = 0.4
 
